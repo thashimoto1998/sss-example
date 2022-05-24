7c0c11ea5e60b9a71624e0b7693ea5589cac4484
Taki <hashimoto19980924@gmail.com>
2022-05-24 13:19:52 +0900

    add enc/dec

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as secrets from "secrets.js-grempe";
+import * as crypto from "crypto";
 
 function main() {
     //const secret = "hello world";
-    const secret = secrets.random(512);
+    const secret = secrets.random(128);
     console.log("secret:", secret, "\n");
     const secretHex = secrets.str2hex(secret);
     
@@ -22,6 +23,22 @@ function main() {
     comb3of5 = secrets.hex2str(comb3of5)
 
     console.log("\ncombined share:", comb3of5);
+
+    const planeText = "This is Plane Text";
+    console.log("plane text:", planeText);
+
+    // Encrypt
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv("aes-256-cbc", secret, iv);
+    let encryptedText = cipher.update(planeText, 'utf8', 'hex');
+    encryptedText += cipher.final('hex');
+    console.log("encrypted text: ", encryptedText);
+
+    // Decrypt
+    const decipher = crypto.createDecipheriv("aes-256-cbc", secret, iv);
+    let decryptedText = decipher.update(encryptedText, 'hex', 'utf-8');
+    decryptedText += decipher.final('utf-8');
+    console.log("decrypted text", decryptedText);
 }
 
 main()
