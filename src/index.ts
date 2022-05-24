a237a44431eb76fc41832cafe3b3efc24f19c27e
Taki <hashimoto19980924@gmail.com>
2022-05-24 11:21:58 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import * as secrets from "secrets.js-grempe";
+
+function main() {
+    //const secret = "hello world";
+    const secret = secrets.random(512);
+    console.log("secret:", secret, "\n");
+    const secretHex = secrets.str2hex(secret);
+    
+    // split into 5 shares, with a threshold of 3, WITH zero-padding
+    const shares3of5 = secrets.share(secretHex, 5, 3, 1024);
+    let count = 1;
+    shares3of5.forEach(s => {
+        console.log("shares", count, ":");
+        console.log(s);
+        count++;
+    });
+
+    // combine 3 shares
+    let comb3of5 = secrets.combine([shares3of5[1], shares3of5[3], shares3of5[4]]);
+
+    // convert back to UTF string
+    comb3of5 = secrets.hex2str(comb3of5)
+
+    console.log("\ncombined share:", comb3of5);
+}
+
+main()
