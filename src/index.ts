0cde28898b570670e539e466b470393b8fe76169
Tatsuki <40402518+thashimoto1998@users.noreply.github.com>
2022-05-25 16:08:16 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import * as secrets from "secrets.js-grempe";
 import * as crypto from "crypto";
+import * as base64 from "js-base64";
+import * as zlib from "zlib";
+import cryptoJs from "crypto-js";
 
 function main() {
     //const secret = "hello world";
-    const secret = secrets.random(128);
+    const secret = secrets.random(64);
     console.log("secret:", secret, "\n");
+    console.log("secret length", secret.length)
     const secretHex = secrets.str2hex(secret);
     
     // split into 5 shares, with a threshold of 3, WITH zero-padding
-    const shares3of5 = secrets.share(secretHex, 5, 3, 1024);
+    const shares3of5 = secrets.share(secretHex, 5, 3);
     let count = 1;
     shares3of5.forEach(s => {
         console.log("shares", count, ":");
@@ -28,17 +32,76 @@ function main() {
     console.log("plane text:", planeText);
 
     // Encrypt
-    const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv("aes-256-cbc", secret, iv);
-    let encryptedText = cipher.update(planeText, 'utf8', 'hex');
-    encryptedText += cipher.final('hex');
+    let encryptedText = cryptoJs.AES.encrypt(planeText, secret).toString();
     console.log("encrypted text: ", encryptedText);
 
     // Decrypt
-    const decipher = crypto.createDecipheriv("aes-256-cbc", secret, iv);
-    let decryptedText = decipher.update(encryptedText, 'hex', 'utf-8');
-    decryptedText += decipher.final('utf-8');
+   let decryptedText = cryptoJs.AES.decrypt(encryptedText, secret).toString(cryptoJs.enc.Utf8);
     console.log("decrypted text", decryptedText);
+
+    
+    // generate key pair
+    const keyPair = crypto.generateKeyPairSync('rsa', {
+        modulusLength: 520,
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem'
+        },
+        privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+        cipher: 'aes-256-cbc',
+        passphrase: 'mySecret'
+        }
+    });
+    console.log("public key", keyPair.publicKey);
+    console.log("buffer", Buffer.from(shares3of5[0]).length);
+    console.log("base64", Buffer.from(base64.encode(shares3of5[0])).length)
+    console.log("gzip", Buffer.from(gzip(shares3of5[0])).length)
+    console.log("share length", shares3of5[0].length);
+    console.log("share buffer", Buffer.from(shares3of5[0].slice(0, 100)).byteLength);
+     
+    console.log("share", shares3of5[0].toString());
+    const encIndexList = [0, 23, 46, 69, 92, 99];
+    let encryptedShareList = [];
+    for (let i = 0; i < 5 ; i++) {
+        const s = shares3of5[0].slice(encIndexList[i], encIndexList[i+1]);
+        const e = crypto.publicEncrypt(
+            keyPair.publicKey,
+            Buffer.from(s)
+        );
+        encryptedShareList.push(e);
+        console.log("share", i, s.toString());
+        console.log("encrypted share", i,  e.toString("base64"));
+        console.log("encrypted share length", e.length);
+    }
+    let encryptedShare = Buffer.concat(encryptedShareList);
+
+    console.log("encrypted share", encryptedShare);
+    console.log("encrypted share length", encryptedShare.length);
+    
+    let decryptedShare = "";
+    for (let i = 0; i < 5; i++) {
+        const e = encryptedShare.slice(i*65, (i+1)*65);
+        console.log("encrypted share", i, e.toString("base64"));
+        const d = crypto.privateDecrypt(
+            {
+                key: keyPair.privateKey,
+                passphrase: 'mySecret'
+            },
+            e
+        );
+        decryptedShare += d.toString();
+        console.log("decrypted share", i, d.toString());
+    }
+    console.log("decrypted share", decryptedShare);
+}
+
+function gzip(str: any){
+    const content = encodeURIComponent(str) // エンコード
+    const result = zlib.gzipSync(content)   // 圧縮
+    const value = result.toString('base64') // Buffer => base64変換
+    return value;
 }
 
 main()
