cae3fc0b19f31407b267151e56ce5c0a9d0d0228
Taki <hashimoto19980924@gmail.com>
2022-08-03 15:29:45 +0900

    verify sig

---
+++ src/sig.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import Web3 from 'web3';
+const Provider = require('@truffle/hdwallet-provider');
+
+const privateKey = process.env.PRIVATE_KEY!;
+const infuraUrl = process.env.INFURA_URL;
+
+const MESSAGE_TO_BE_SIGNED = "Hello VWBL";
+
+async function main() {
+    console.log(privateKey)
+    const provider = new Provider(privateKey, infuraUrl);
+    const web3 = new Web3(provider);
+    const myAddress = (await web3.eth.getAccounts())[0];
+
+    console.log("address: ", myAddress);
+    const sig = await web3.eth.personal.sign(MESSAGE_TO_BE_SIGNED, myAddress, ""); 
+    console.log("sig: ", sig);
+}
+
+main()
